9b19fce10dc90321eb47ab35393ac7231cb24b59
Zulfuqar Asgerzade <asgerzade.zulfuqar@gmail.com>
2019-12-16 15:15:52 +0400

    Main Page components

---
+++ my-app/src/components/Navbar/navbar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Logo from "../MainPage/logo";
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <Logo/>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
