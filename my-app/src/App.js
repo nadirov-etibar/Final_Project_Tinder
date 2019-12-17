dd9847eb472bfe30e1cf8f103a3ed88708ddd849
KananValizada <kenan.veli@yahoo.com>
2019-12-17 21:25:48 +0400

    Swipe added node updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import MainPage from "./components/MainPage/mainPage";
 import Profile from "./components/Profile/profile";
+import Deck from "./components/Deck";
 
 function App() {
   return (
-    <div className="App">
-      {/*<MainPage/>*/}
+    <div className="App"> 
+      <MainPage/>
       <Profile/>
+      <Deck />
+    
     </div>
   );
 }
