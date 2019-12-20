f788143ce0531ad0c0964f8327e75461212188b9
nadirov-etibar <nadirov.etibar@gmail.com>
2019-12-20 16:03:50 +0400

    changes profile

--- my-app/src/components/Profile/profile.js
+++ my-app/src/components/Profile/profile.js
@@ -24,11 +24,13 @@ class Profile extends Component {
                         <div className={"profile__bottom"}>
                            <Button classes={btnClasses} value={"like"} info={"Like"}/>
                             <form action="" className={"profile__form"}>
-                            <label htmlFor="file" className="profile__circle-btn-blue"><svg className="Sq(32px) Sq(28px)--xs profile__camera" viewBox="0 0 24 24" width="24px" height="24px"
+                            <label htmlFor="file" className="profile__circle-btn-blue"><svg className="Sq(32px) Sq(28px)--xs profile__camera" viewBox="0 0 24 24" width="30px" height="30px"
                                                                                             focusable="false" aria-hidden="true" role="presentation">
                                 <path className="Fill(#fff)"
                                       d="M4,4H7L9,2H15L17,4H20A2,2 0 0,1 22,6V18A2,2 0 0,1 20,20H4A2,2 0 0,1 2,18V6A2,2 0 0,1 4,4M12,7A5,5 0 0,0 7,12A5,5 0 0,0 12,17A5,5 0 0,0 17,12A5,5 0 0,0 12,7M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9Z"/>
-                            </svg></label>
+                            </svg>
+                                <div className={"profile__plus"}></div>
+                            </label>
                             <Input type={'file'} id={'file'}/>
                             <h2 className={"profile__info-btn-blue"}>Upload Photo</h2>
                             </form>
